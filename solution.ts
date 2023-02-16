29c0440862cee997d12d90a15e429320eea956b6
Kooreis <kooreis.official@gmail.com>
2023-02-16 11:02:49 -0600

    This update introduces the mergeIntervals function. This function sorts the intervals by their start times and then iterates through them, merging any that overlap. The merged intervals are added to a new array, which is returned at the end.

--- solution.ts
+++ solution.ts
@@ -1,9 +1,25 @@
-class Interval {
-    start: number;
-    end: number;
-
-    constructor(start: number, end: number) {
-        this.start = start;
-        this.end = end;
+function mergeIntervals(intervals: Interval[]): Interval[] {
+    if (intervals.length <= 1) {
+        return intervals;
     }
+
+    intervals.sort((a, b) => a.start - b.start);
+
+    let result: Interval[] = [];
+    let start = intervals[0].start;
+    let end = intervals[0].end;
+
+    for (let i = 1; i < intervals.length; i++) {
+        if (intervals[i].start <= end) {
+            end = Math.max(end, intervals[i].end);
+        } else {
+            result.push(new Interval(start, end));
+            start = intervals[i].start;
+            end = intervals[i].end;
+        }
+    }
+
+    result.push(new Interval(start, end));
+
+    return result;
 }
